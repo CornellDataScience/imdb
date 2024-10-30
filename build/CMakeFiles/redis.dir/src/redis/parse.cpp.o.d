fcac044af9ac4c48086377acdcd3ca26094038fb
Chris <cx72@cornell.edu>
2024-10-30 18:44:07 -0400

    remove build from gitignore

--- build/CMakeFiles/redis.dir/src/redis/parse.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/redis.dir/src/redis/parse.cpp.o: \
-  /Users/chris/Documents/Cornell/CDS/24-25/imdb/src/redis/parse.cpp
--- build/CMakeFiles/redis.dir/src/redis/store.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/redis.dir/src/redis/store.cpp.o: \
-  /Users/chris/Documents/Cornell/CDS/24-25/imdb/src/redis/store.cpp
