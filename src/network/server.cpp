50e9f47a32d054fa754ebd98f1ac91b913141036
Tanvi <tjb279@cornell.edu>
2024-11-06 21:32:24 -0500

    Added socket includes

--- src/network/server.cpp
+++ src/network/server.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
 
 int main()
 {
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    int buffer[1024];
+    if(listening == -1){
+        std::cerr << "no socket for you"<< std::endl;
+        return -1;
+    }
     std::cout << "TCP Test\n";
     return 0;
 }
