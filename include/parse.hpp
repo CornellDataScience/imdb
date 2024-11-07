f8c151d9ec80fc9cab09d08ea834b7e03a97de1f
Chris <cx72@cornell.edu>
2024-11-06 22:34:38 -0500

    Headers for parse

--- include/parse.hpp
+++ include/parse.hpp
@@ -1,4 +1,23 @@
+#pragma once
 
+#include <cstdint>
+#include <string>
 
-void read(char* shm_buff, int shm_start, int shm_size, char* buff);
-void write(char* shm_buff, int shm_start, int shm_size, char* buff);
+enum class RespType : uint8_t
+{
+    ARRAY = 0,
+    STRING = 1
+};
+
+struct Message
+{
+    uint8_t type;
+    std::string key;
+    std::string val;
+};
+
+Message deserialize(char *buff);
+void parseRespArray(char *buff, Message &msg);
+std::string parseRespArray(char *buff);
+std::string parseBulkString(char *&buff);
+bool serialize(const Message &msg, char *buff);
